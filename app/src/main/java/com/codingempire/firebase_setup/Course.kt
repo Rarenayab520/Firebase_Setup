ed2431dd91845e23e31aa449e6b872c5525e6573
Rarenayab520 <nayabasmat121@gmail.com>
2025-08-22 12:02:11 +0100

    Course Collection

---
+++ app/src/main/java/com/codingempire/firebase_setup/Course.kt
@@ -0,0 +1,8 @@
+package com.codingempire.firebase_setup
+
+data class Course(
+    val rollNo : String = "",
+    val course: String = "",
+    val creditHours: String = "",
+    val teacherName: String = ""
+)
--- app/src/main/java/com/codingempire/firebase_setup/HomeActivity.kt
+++ app/src/main/java/com/codingempire/firebase_setup/HomeActivity.kt
@@ -2,16 +2,21 @@ package com.codingempire.firebase_setup
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.codingempire.firebase_setup.databinding.ActivityHomeBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -21,7 +26,28 @@ class HomeActivity : AppCompatActivity() {
         binding.tvRecieved.text = "WELCOME , ${user?.name ?: "Guest"}"
 
 
+       binding.btnSave.setOnClickListener {
+           val rollNo = binding.etRollNo.text.toString().trim()
+           val course = binding.etCourse.text.toString().trim()
+           val creditHours = binding.etCreditHours.text.toString().trim()
+           val teacherName = binding.etCourseTeacher.text.toString().trim()
 
+           if(rollNo.isEmpty() || course.isEmpty() || creditHours.isEmpty() || teacherName.isEmpty()){
+               Toast.makeText(this,"Please fill all the fields",Toast.LENGTH_SHORT).show()
+               return@setOnClickListener
+           }
+
+           val newCourse = Course(rollNo,course,creditHours,teacherName)
+
+        val savedCourse =  db.collection("Courses")
+           savedCourse.add(newCourse)
+               .addOnSuccessListener {
+                   Toast.makeText(this,"Course Data Saved",Toast.LENGTH_SHORT).show()
+               }.addOnFailureListener {
+                   Toast.makeText(this,"Error...!Course Data not Saved",Toast.LENGTH_SHORT).show()
+               }
+
+       }
 
 
         binding.btnLogout.setOnClickListener {
